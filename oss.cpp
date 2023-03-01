b37d54cee6124c1824e38c7c9524a634b65601b1
matthew knibb <knibb@hoare7.cs.umsl.edu>
2023-02-28 23:19:46 -0600

    partial shared mem setup

--- oss.cpp
+++ oss.cpp
@@ -9,75 +9,74 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/types.h>
-#include <fcntl.h>
-#include <semaphore.h>
-#include <errno.h>
+
 
 using namespace std;
 
 int main(int argc, char** argv){
-    int opt;
-    //remember that if you want an argument to accept a value you have to have a : after it
-    //example[opt = getopt(argc, argv, ":hn:s")) <- the n: is now going to expect a vlaue]
-    int max_Sec = 0;
-    int max_Nano = 0;
-    int sim = 0;
-    int proc = 0;
-    //while loop that takes in user provided arguments that then get given to variables: proc, sim, iter.
+	memIDSeconds = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0640);
+	sysClockSeconds = (int*) shmat(memIDSeconds, (void*) 0, 0);
+        int opt;
+        //remember that if you want an argument to accept a value you have to have a : after it
+        //example[opt = getopt(argc, argv, ":hn:s")) <- the n: is now going to expect a vlaue]
+      	int sec = 0;
+        int nano = 0;
+        int sim = 0;
+        int proc = 0;
+        //while loop that takes in user provided arguments that then get given to variables: proc, sim, iter.
         //proc will house the number of processes to launch
         //sim will house how many simultaneous proccess that will be ran at a time
         //iter will house how many iterations of the workers
         while((opt = getopt(argc, argv, "hn:s:t:")) != -1) {
-                switch(opt) {
-                        case 'h':
-                                printf("syntax: oss -[h] -[n] -[s] -[t]\n");
-                                printf("example: oss -n7 -s3 -t5.\n");
-                                printf("-h: Display help prompt.\n");
-                                printf("-n: specifies the number of total processes the user wants launched.\n");
-                                printf("-s: specifies the number of processes that can be running at the same time.\n");
-                                printf("-t: specifies the amount of time the process should live.\n");
-                                exit(0);
-                        case 'n':
-                        //      printf("%s\n", optarg);
-                                proc = atoi(optarg);
-                                break;
-                        case 's':
-                        //      printf("%s\n", optarg);
-                                sim = atoi(optarg);
-                                break;
-                        case 't':
-                        //      printf("%s\n", optarg);
-                                max_Sec = atoi(optarg);
-                                max_Nano = atoi(optarg);
-                                break;
-                        case '?':
-                                printf("unknown option");
-                                break;
-                }
-        }
-
-	struct PCB {
-		int occupied; // either true or false
-		pid_t pid; // process id of this child
-		int startSeconds; // time when it was forked
-		int startNano; // time when it was forked
-	};
-
-	struct PCB processTable[20];
-	int sec = 0;
-	int nano = 0;
-	while(max_Sec > sec && max_Nano > nano) {
-		nano = nano + 1000;
-		if(nano >= 1000000000){
-			sec = sec + 1;
-			nano = 0;
+        switch(opt) {
+        	case 'h':
+                	printf("syntax: oss -[h] -[n] -[s] -[t]\n");
+                	printf("example: oss -n7 -s3 -t5.\n");
+        		printf("-h: Display help prompt.\n");
+        		printf("-n: specifies the number of total processes the user wants launched.\n");
+                	printf("-s: specifies the number of processes that can be running at the smae time.\n");
+                	printf("-t: specifies the number of iterations the worker should perform.\n");
+                	exit(0);
+                case 'n':
+                        printf("%s\n", optarg);
+                        proc = atoi(optarg);
+                        break;
+                case 's':
+        		printf("%s\n", optarg);
+                        sim = atoi(optarg);
+                        break;
+                case 't':
+        		printf("%s\n", optarg);
+                        sec = atoi(optarg);
+                        break;
+                case '?':
+                        printf("unknown option");
+                        break;
+        	}
+	}
+	int tracker = 0;
+	//create child proccesses
+	for (int count=0;count < proc; count++){
+		int pid = fork();
+		if (pid == 0){
+		//if pid is 0 then child
+			std::stringstream ss;
+			ss<<max_Sec;
+			execl("./worker","./worker",ss.str().c_str() ,NULL);
+		}
+		//increment tracker to keep track of proccesses
+		tracker++;
+		if(tracker >= sim){
+		//if there are more processes then the specified amount then wait for a proccess to end
+		wait(NULL);
+		tracker--;
 		}
 	}
-
-	
-
-
-
-
-
-}
+	//insures there are no orphans by waiting for all processes to die
+	for (int i = 0; i<tracker;i++){
+	wait(NULL);
+	}
+shmdt(sysClockSeconds);
+shmctl(memIDSeconds, IPC_RMID, NULL);
+return 0;
+}	
--- worker.cpp
+++ worker.cpp
@@ -6,16 +6,11 @@
 
 int main(int argc, char** argv){
 	//convert the char to an int for the second arg
-	int iter = atoi(argv[1]);
-	for (int i=1, int n=0;i<=sec & n <= nano;i++){
-		void *shared_mem;
-		char buff[100];
-		int shmid;
-		shmid = shmget((key_t)1616, 1024, 0666);
-		shared_mem = shmat(shmid,NULL,0);
-		printf("WORKER PID: %d PPID: %d Iteration: %d before sleeping\n", getpid(), getppid(), i);
-		sleep(1);
-		printf("WORKER PID: %d PPID: %d Iteration: %d after sleeping\n", getpid(), getppid(), i);
+	int max_Sec = atoi(argv[1]);
+	for (int i=1;i<=max_Sec;i++){
+	printf("WORKER PID: %d PPID: %d Iteration:%d before sleeping\n", getpid(), getppid(), i);
+	sleep(1);
+	printf("WORKER PID: %d PPID: %d Iteratoin:%d after sleeping\n", getpid(), getppid(), i);
 	}
 	return 0;
 }
